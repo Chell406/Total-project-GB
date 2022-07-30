9522eaaa11483656ee5afbd6e4cf8cdfe3c64a73
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-07-30 14:53:03 +0300

    Исправил Lenght на Length в именах переменных

--- Code.cs
+++ Code.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string[] inputMassOfStrings = {"antonim", "ant", "mole", "pow"};
+int inputMassLength = 4;
+int outputMassLength = 0;
+int inputMassIndex = 0;
+int outputMassIndex = 0;
+
+while(inputMassIndex < 4)
+{
+    if(inputMassOfStrings[inputMassIndex].Length <= 3)
+}
