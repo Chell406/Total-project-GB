eda01fb71153bdec470cef49400be69fbf5d7797
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-07-30 15:50:59 +0300

    Выделил решение в отдельный метод

--- Code.cs
+++ Code.cs
@@ -1,29 +1,37 @@
-﻿string[] inputMassOfStrings = {"antonim", "ant", "mole", "pow"};
-int inputMassLength = 4;
-int outputMassLength = 0;
-int inputMassIndex = 0;
-int outputMassIndex = 0;
+﻿string[] massOfStrings = { "antonim", "ant", "mole", "pow"};
+int massLength = 4;
 
-while(inputMassIndex < inputMassLength)
+string[] StringsLessThenThree(string[] inputMassOfStrings, int inputMassLength)
 {
-    if(inputMassOfStrings[inputMassIndex].Length <= 3)
-        outputMassLength = outputMassLength + 1;
-    
-    inputMassIndex = inputMassIndex + 1;
-}
+    int outputMassLength = 0;
+    int inputMassIndex = 0;
+    int outputMassIndex = 0;
 
-string[] outputMassOfStrings = new string[outputMassLength];
-
-inputMassIndex = 0;
-
-while(outputMassIndex < outputMassLength)
-{
-    if(inputMassOfStrings[inputMassIndex].Length <= 3)
+    while (inputMassIndex < inputMassLength)
     {
-        outputMassOfStrings[ outputMassIndex ] =   inputMassOfStrings[ inputMassIndex ];
-        outputMassIndex = outputMassIndex + 1;
+        if (inputMassOfStrings[inputMassIndex].Length <= 3)
+            outputMassLength = outputMassLength + 1;
+
+        inputMassIndex = inputMassIndex + 1;
     }
 
-    inputMassIndex = inputMassIndex + 1;
+    string[] outputMassOfStrings = new string[outputMassLength];
 
+    inputMassIndex = 0;
+
+    while (outputMassIndex < outputMassLength)
+    {
+        if (inputMassOfStrings[inputMassIndex].Length <= 3)
+        {
+            outputMassOfStrings[outputMassIndex] = inputMassOfStrings[inputMassIndex];
+            outputMassIndex = outputMassIndex + 1;
+        }
+
+        inputMassIndex = inputMassIndex + 1;
+
+    }
+
+    return outputMassOfStrings;
 }
+
+string[] result = StringsLessThenThree(massOfStrings, massLength);
