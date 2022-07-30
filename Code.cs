63eaff7935b58cdac556b27f65869bcccc3f06f3
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-07-30 15:06:27 +0300

    Реализован код согласно блок-схеме

--- Code.cs
+++ Code.cs
@@ -4,7 +4,26 @@ int outputMassLength = 0;
 int inputMassIndex = 0;
 int outputMassIndex = 0;
 
-while(inputMassIndex < 4)
+while(inputMassIndex < inputMassLength)
 {
     if(inputMassOfStrings[inputMassIndex].Length <= 3)
+        outputMassLength = outputMassLength + 1;
+    
+    inputMassIndex = inputMassIndex + 1;
+}
+
+string[] outputMassOfStrings = new string[outputMassLength];
+
+inputMassIndex = 0;
+
+while(outputMassIndex < outputMassLength)
+{
+    if(inputMassOfStrings[inputMassIndex].Length <= 3)
+    {
+        outputMassOfStrings[ outputMassIndex ] =   inputMassOfStrings[ inputMassIndex ];
+        outputMassIndex = outputMassIndex + 1;
+    }
+
+    inputMassIndex = inputMassIndex + 1;
+
 }
